4eb6cc9347b7ef350a28977adc3721de94597140
biaoabiao <784838018@qq.com>
2020-11-23 18:37:45 +0800

    完成taskTypyInfo

--- babTest/testcase/testTaskTypeInfo.py
+++ babTest/testcase/testTaskTypeInfo.py
@@ -9,10 +9,12 @@ from testFile.geturlParams import geturlParams
 from testFile.readConfig import ReadConfig
 from testFile.readExcel import readExcel
 from common.configHttp import RunMain
+from common.util import Utility
 
 
 readconfig = ReadConfig()
-login_xls = readExcel().get_xls('userCase.xlsx','login')
+login_xls = readExcel().get_xls('userCase.xlsx','taskTypeInfo')
+util = Utility()
 
 class testTaskTypeInfo(unittest.TestCase):
     def setUp(self):
@@ -23,7 +25,28 @@ class testTaskTypeInfo(unittest.TestCase):
         print('测试开始前的准备')
 
     def test_get_longvoice_list(self):
-        pass
+        self.checkResult()
+
+    def checkResult(self):
+        url = geturlParams().get_url(readconfig.get_http('baseurl_first'),
+                                     self.request_path)  # 调用我们的geturlParams获取我们拼接的url
+        new_url = url + self.request_data
+        data = dict(urllib.parse.parse_qsl(
+            urllib.parse.urlsplit(
+                new_url).query))  # 将一个完整的URL中的name=&password=转换为{'username':'xxx','password':'bbb'}
+        # 调用util进行加密
+        username = util.md5_join_b64(data["user_name"])
+        user_pwd = util.md5_join_b64(data["user_pwd"])
+        # 把加密后的值在替换到相应的位置
+        data["user_name"] = username
+        data["user_pwd"] = user_pwd
+        info = RunMain().run_main(self.request_method, url, data,
+                                  files=None)  # 根据Excel中的method调用run_main来进行requests请求，并拿到响应
+        ss = info.json()  # 根据Excel中的method调用run_main来进行requests请求，并拿到响应
+        if self.case_name == 'login_success':  # 如果case_name是login，说明合法，返回的code应该为200
+            self.assertEqual(self.expect_code, ss['code'])
+        if self.case_name == 'login_error':  # 同上
+            self.assertEqual(self.expect_code, ss['code'])
 
 
 
